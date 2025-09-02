eba2b6386d4501c9a0e1dc8dcccf885dbdd42905
aa <aaa>
2025-09-02 12:23:10 +0900

    0902yuuki

--- src/main/java/jp/te4a/teamc/spring/boot/bookapp/config/SecurityConfig.java
+++ src/main/java/jp/te4a/teamc/spring/boot/bookapp/config/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig implements WebMvcConfigurer {
                 .formLogin(login -> login
                         .loginPage("/loginForm")
                         .loginProcessingUrl("/login")
-                        .usernameParameter("user")
+                        .usernameParameter("username")
                         .passwordParameter("password")
                         .defaultSuccessUrl("/orderhistory", true)
                         .failureUrl("/loginForm?error")
